cd1c663339977a951ab51689b9c55c9786342ebc
dorn-gerhard <quantum.quentin@mailbox.org>
2024-06-08 17:54:22 +0200

    add scroll

--- Assets/Scripts/Graphics/Function.cs
+++ Assets/Scripts/Graphics/Function.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class Function : MonoBehaviour
 {
@@ -11,6 +13,12 @@ public class Function : MonoBehaviour
     public Slider rotationSlider;
     public Slider parameter1;
     public Slider parameter2;
+    public Projectile projectile;
+
+    public int numberOfPoints = 200;
+    float startValue = -2.0f;
+    float endValue = 2.0f;
+    float angle = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,29 +36,46 @@ public class Function : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Draw();
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnProjectile();
+        }
+        angle = Math.Clamp(angle + Input.GetAxis("Mouse ScrollWheel") * 180.0f, -180.0f, 180.0f);
+
     }
 
-
-    public void Draw()
+    public void SpawnProjectile()
     {
-        Debug.Log("Parameter: " + rotationSlider.value);
-        int numberofpoints = 200;
-        float startValue = -10.0f;
-        float endValue = 10.0f;
-        float increment = (endValue - startValue) / (numberofpoints+1);
-        float[] parameterVals = new float[numberofpoints];
-        float[] functionVals = new float[numberofpoints];
-        Vector3[] vertexPositions = new Vector3[numberofpoints];
+        List<Vector3> vec = GetPath(0.0f, endValue).ToList();
+        Instantiate(projectile).InitializePath(vec);
 
-        for (int i = 0; i < numberofpoints; i++)
+        List<Vector3> vec2 = GetPath(0.0f, startValue).ToList();
+        Instantiate(projectile).InitializePath(vec2);
+    }
+
+    private Vector3[] GetPath(float startValue, float endValue)
+    {
+        
+        float increment = (endValue - startValue) / (numberOfPoints + 1);
+        float[] parameterVals = new float[numberOfPoints];
+        float[] functionVals = new float[numberOfPoints];
+        Vector3[] vertexPositions = new Vector3[numberOfPoints];
+
+        for (int i = 0; i < numberOfPoints; i++)
         {
 
             float yValue = EvalFunction(startValue + i * increment, parameter1.value, parameter2.value);
             Vector3 temp_vector = new Vector3(startValue + i * increment, yValue, 0);
-            vertexPositions[i] = Quaternion.AngleAxis(rotationSlider.value, Vector3.up) * temp_vector; 
+            vertexPositions[i] = Quaternion.AngleAxis(angle, Vector3.forward) * temp_vector;
         }
-        DrawLine(numberofpoints, vertexPositions);
+        return vertexPositions;
+    }
+
+    public void Draw()
+    {
+        Vector3[] vertexPositions = GetPath(startValue, endValue);
+        DrawLine(numberOfPoints, vertexPositions);
 
     }
     void DrawLine(int nPoints, Vector3[] vertexPositions)
@@ -63,5 +88,10 @@ public class Function : MonoBehaviour
     float EvalFunction(float x, float param1, float param2)
     {
         return param2 * (float)Math.Sin(param1 * x);
+    }
+
+    public void UpdateAngle()
+    {
+        angle = rotationSlider.value;
     }
 }
