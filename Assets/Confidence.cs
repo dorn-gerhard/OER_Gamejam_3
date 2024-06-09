333649dcc642eb82575f412b715f2589eaf43451
Boris Asenov <boris1asenov@gmail.com>
2024-06-09 13:27:20 +0200

    Make lazer into seperate prefab

--- Assets/Confidence.cs
+++ Assets/Confidence.cs
@@ -11,9 +11,37 @@ public class Confidence : MonoBehaviour
     public Image fillBar;
     public TMP_Text procentageText;
 
+    public float decayDelay = 3;
+    public float decayAmount = 10;
+    float timeSinceLastDelay = 0;
+
     public void UpdateConfidenceUI()
     {
         fillBar.fillAmount = confidence / 100;
         procentageText.text = confidence.ToString("F0") + "%";
     }
+
+    private void Update()
+    {
+        Decay();
+    }
+
+    void Decay()
+    {
+        if (gameObject.activeSelf && gameObject.GetComponent<CanvasGroup>().alpha < 1)
+        {
+            timeSinceLastDelay += Time.deltaTime;
+            if (timeSinceLastDelay >= decayDelay)
+            {
+                confidence -= decayAmount;
+                UpdateConfidenceUI();
+                timeSinceLastDelay = 0;
+            }            
+        }
+    }
+
+    //void ChangeConfidence
+    //{
+
+    //}
 }
