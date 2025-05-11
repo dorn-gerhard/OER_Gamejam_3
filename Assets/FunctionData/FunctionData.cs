6ea9bdf02ff8cd2dc7de298a6e5c08bd59ac2e62
imaspohi <ida.stetter@gmail.com>
2025-05-11 11:55:49 +0200

    Add Funcion reference word document

--- Assets/FunctionData/FunctionData.cs
+++ Assets/FunctionData/FunctionData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FunctionData", menuName = "ScriptableObjects", order = 1)]
 public class FunctionData : ScriptableObject
 {
-    [SerializeField] public string functionAttributes;
+    [SerializeField][TextArea(3,10)] public string functionAttributes;
     [SerializeField] public Sprite functionGraph;
     [SerializeField] public bool is_correct;
 
