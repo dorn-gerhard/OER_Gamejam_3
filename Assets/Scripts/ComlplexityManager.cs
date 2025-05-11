ceae97f3dd05f74f239b2fda312870d0f995a7da
Olga_Gor <gorbunova@student.tugraz.at>
2025-05-11 14:29:24 +0200

    did the same thing

--- Assets/Scripts/ComlplexityManager.cs
+++ Assets/Scripts/ComlplexityManager.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComlplexityManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+public class ComplexityManager : MonoBehaviour
+{
+    public static ComplexityManager Instance { get; private set; }
+
+    public enum Difficulty
     {
-        
+       EASY,
+       MEDIUM,
+       HARD
+    } // stores which complexity was chosen
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Keep this object when changing scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy duplicates if one already exists
+        }
     }
 }
