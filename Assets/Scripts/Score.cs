be2c0cf84ce9dc242c2a1ce0916e9811649bfd7b
Gerhard Dorn <quantum.quentin@mailbox.org>
2025-05-11 16:21:19 +0200

    upload sounds

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -11,22 +11,32 @@ public class Score : MonoBehaviour
     [SerializeField] int ScoreMultiplier = 1;
     [SerializeField] int PenaltyMultiplier = 1;
 
+    private AudioSource audioSource;
+    public AudioClip failClip;
+    public AudioClip successClip;
+
     public static Score Instance;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+
         Instance = this;
         Update();
     }
 
     public void Add(int points)
     {
+        audioSource.PlayOneShot(successClip, .6f);
+
         currentScore += ScoreMultiplier * points;
         Update();
     }
 
     public void Punish(int points)
     {
+        audioSource.PlayOneShot(failClip);
+
         currentScore -= PenaltyMultiplier * points;
         Update();
     }
