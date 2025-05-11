3d2240cdfe91c0e9808350178bb82467cfafd278
imaspohi <ida.stetter@gmail.com>
2025-05-11 16:08:50 +0200

    Turn radio on again

--- Assets/Scripts/radioButton.cs
+++ Assets/Scripts/radioButton.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class radioButton : MonoBehaviour
 {
     [SerializeField] private AudioSource BackgroundMusic;
-
+    private bool music_is_off;
+    private EventSystem eventSystem;
+    private void Start()
+    {
+        eventSystem = EventSystem.current;
+    }
     public void turnOff()
     {
-        BackgroundMusic.volume =0f;
+        eventSystem.SetSelectedGameObject(null);
+        if (!music_is_off)
+        {
+            BackgroundMusic.volume = 0f;
+            music_is_off = true;
+        }
+        else
+        {
+            BackgroundMusic.volume = 1f;
+            music_is_off=false;
+        }
+
     }
 }
