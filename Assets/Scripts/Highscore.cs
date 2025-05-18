014722ec0373f61a8816813f9bcc7eccb545fdc7
guffab <guflerfabian02@gmail.com>
2025-05-18 19:49:13 +0200

    copy pre-filled highscores onto local builds

--- Assets/Scripts/Highscore.cs
+++ Assets/Scripts/Highscore.cs
@@ -1,17 +1,27 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
 public class Highscore : MonoBehaviour
 {
-    static string FilePath => Path.Combine(UnityEngine.Application.persistentDataPath, "Highscores.json");
+    static string FilePath => Path.Combine(UnityEngine.Application.persistentDataPath, Filename);
     [SerializeField] TMP_Text HighscorePlayers;
     [SerializeField] TMP_Text HighscoreScores;
+
+    const string Filename = "Highscores.json";
+    
+    void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            WebGLFileLoader.Init(this, Filename);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -26,22 +36,14 @@ public class Highscore : MonoBehaviour
             HighscorePlayers.text += highscore.Name + "\n";
             HighscoreScores.text += highscore.Score + "\n";
         }
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public static void AddHighscore(string username, int score)
     {
         List<Entry> entries = GetHighscores();
         
-        if (score > 0)
-        {
+        if (score != 0)
             entries.Add(new Entry(username, score));
-        }
 
         var json = JsonConvert.SerializeObject(entries);
         File.WriteAllText(FilePath, json);
@@ -49,14 +51,26 @@ public class Highscore : MonoBehaviour
 
     public static List<Entry> GetHighscores()
     {
-        string path = FilePath;
-        var jsonFile = File.Exists(path) ? File.ReadAllText(path) : "";
-
+        var jsonFile = GetSaveFile();
         var entries = JsonConvert.DeserializeObject<List<Entry>>(jsonFile) ?? new();
 
         //sort highest to lowest
         entries.Sort((a, b) => (b.Score.CompareTo(a.Score)));
         return entries;
+    }
+
+    private static string GetSaveFile()
+    {
+        //local highscores
+        if (File.Exists(FilePath))
+            return File.ReadAllText((FilePath));
+
+        //load pre-filled highscore for extra motivation
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            return WebGLFileLoader.GetContents();
+        
+        var fullPath = Path.Combine(Application.streamingAssetsPath, Filename);
+        return File.ReadAllText(fullPath);
     }
 
     [Serializable]
@@ -68,4 +82,39 @@ namespace System.Runtime.CompilerServices
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     internal class IsExternalInit { }
+}
+
+//for WebGL, files need to be streamed into, direct loading of files is not possible
+public static class WebGLFileLoader
+{
+    private static string _cachedContents;
+    private static bool _isLoaded;
+
+    public static void Init(MonoBehaviour context, string filename)
+    {
+        if (_isLoaded) return;
+        context.StartCoroutine(LoadFile(filename));
+    }
+
+    public static string GetContents()
+    {
+        return _isLoaded ? _cachedContents : "";
+    }
+
+    private static IEnumerator LoadFile(string filename)
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, filename);
+        using UnityWebRequest uwr = UnityWebRequest.Get(path);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result == UnityWebRequest.Result.Success)
+        {
+            _cachedContents = uwr.downloadHandler.text;
+            _isLoaded = true;
+        }
+        else
+        {
+            Debug.LogError($"[WebGLFileLoader] Failed to load file: {uwr.error}");
+        }
+    }
 }
